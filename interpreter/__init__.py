1c77d59f9d0ef2d9234b57801acce944dc46ba6c
chumba-wamba <nisarg0271@gmail.com>
2020-09-21 23:56:51 +0530

    Added lang details to interpreter/ __init__.py

---
+++ interpreter/__init__.py
@@ -0,0 +1,22 @@
+"""
+- lexer.py
+    - <class> Token :: serves as a structure to store metadata such as line number, character number, operator data, etc.
+    - <class> Lexer :: generates tokens and populates the brainfxck storage tape with these tokens.
+- parser.py
+    - <class> Parser :: responsbile for parsing and interpreting the code; generating error messages and other information.
+
+- rules (refer https://gist.github.com/roachhd/dce54bec8ba55fb17d3a)
+    - any arbitrary character besides the 8 listed below should be ignored and considered as comments.
+    - all the blocks on the memory tape are intialised with a zero and the tape pointer starts from the left-most position. 
+    - loops may be nested as many times as one wants but every "[" must have a corresponding "]".
+
+- inforamtion on brainfxck operators
+    > :: moves memory pointer to the right of the current position.
+    < :: moves memory pointer to the left of the current position.
+    + :: increments the memory block value by one.
+    - :: decrements the memory block value by one.
+    [ :: like c while(cur_block_value != 0) loop.
+    ] :: if block currently pointed to's value is not zero, jump back to "[".
+    , :: used to input a character.
+    . :: used to output a character.
+"""
