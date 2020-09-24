230fae7a39ed36f985eae923b65cd3c6fb634ffe
chumba-wamba <nisarg0271@gmail.com>
2020-09-25 00:49:49 +0530

    Added more comments, doctstrings, etc

--- unit_tests.py
+++ unit_tests.py
@@ -9,12 +9,9 @@ if __name__ == "__main__":
     lex = Lexer(code)
     token_stream = lex.lex()
 
-    for token in token_stream:
-        print(token.op_name)
+    print(token_stream, end="\n\n\n")
 
     syn_anal = SyntaxAnalyser(token_stream=token_stream)
     fault_dict = syn_anal.analyse()
 
-    print(fault_dict["faulty_token_list"])
-    for token in fault_dict["faulty_token_list"]:
-        print(token.op_name, token.line_number, token.posn_number)
+    print(fault_dict)
