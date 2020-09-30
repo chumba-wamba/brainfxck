756ec8b7f416dc5198e58a3acca5bc78363e3520
chumba-wamba <nisarg0271@gmail.com>
2020-09-30 18:00:49 +0530

    Documented source code via comments :bulb:

--- API/api_client.py
+++ API/api_client.py
@@ -3,6 +3,8 @@ from json import loads, dumps
 from typing import Dict
 
 
+# NOTE - Is to be updated according to the URL at
+# which the brainfxck API is hosted at
 url = "http://localhost:8000/"
 
 
@@ -31,8 +33,15 @@ def eval_code(file: str) -> str:
 
     with open(file) as f:
         code = f.read()
+    # The most important aspect of this function; replaces
+    # all the newline characters in the file with a raw string
+    # containing \n so that it becomes comaptible with JSON and
+    # can be evaluated by the API
     code = code.replace("\n", r"\n")
 
+    # Creating a dictionary which is compatible with the
+    # body expected by the API and using the dumps method to
+    # make it JSON compatible
     data = {'code': code}
     response = post(url+"evaluate", data=dumps(data))
 
--- API/server.py
+++ API/server.py
@@ -8,6 +8,12 @@ app = FastAPI()
 
 @app.get("/")
 def root():
+    """
+        End point to send a simple response to any get
+        request; the main purpose of this is to check 
+        whether the API is up and running or not.
+    """
+
     return {
         "success": True,
         "message": "An API to interpret your brainfxck code!",
@@ -17,9 +23,15 @@ def root():
 
 @app.post("/evaluate")
 def eval(code_in: CodeIn):
+    """
+        End point to evaluate the brainfxck code that is
+        recieved as a post request with the help of the
+        interpreter.interpreter.evaluate function.
+    """
+
     output = evaluate(code_in.code)
     return {
         "success": True,
-        "message": "Code successfully executed!",
+        "message": "Code successfully evaluated by the interpreter.",
         "data": CodeOut(output=output)
     }
